bfa45a71e883eab15c986f576559945f4dd66771
Gkim <Grigoriy.kim1@gmail.com>
2023-04-24 14:51:02 +0300

    Initial commit

---
+++ src/main/java/com/gkim/spring/springboot/spring_data_jpa/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.gkim.spring.springboot.spring_data_jpa.dao;
+
+
+import com.gkim.spring.springboot.spring_data_jpa.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    public List<Employee> findAllByName(String name);
+}
